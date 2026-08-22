549811c3ae01493fa7e52b22e56e40330af0549a
Priscila Durão <112986283+PriscilaDurao@users.noreply.github.com>
2026-08-22 19:23:20 -0300

    estruturação projeto

---
+++ src/utils/list.ts
@@ -0,0 +1,21 @@
+import instagram from "../../src/assets/icons/instagram.svg";
+import facebook from "../../src/assets/icons/facebook.svg";
+import twitter from "../../src/assets/icons/twitter.svg";
+
+export const SocialLinks = [
+  {
+    id: 1,
+    title: "icone do instagram",
+    icon: instagram,
+  },
+  {
+    id: 2,
+    title: "icone do facebook",
+    icon: facebook,
+  },
+  {
+    id: 3,
+    title: "icone do twitter",
+    icon: twitter,
+  },
+];
